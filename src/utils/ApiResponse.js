cc2bee42943236dde847e818972080fbeb809819
Ishika <ishikadas414@gmail.com>
2025-03-31 12:40:16 +0530

    added register controller

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -8,6 +8,7 @@ class ApiResponse {
                                         // Redirection messages (300 – 399)
                                         // Client error responses (400 – 499)
                                         // Server error responses (500 – 599)
-        this.errors = errors;
     }
 }
+
+export { ApiResponse };
